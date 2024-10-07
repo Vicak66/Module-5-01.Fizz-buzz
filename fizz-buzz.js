430110a4981caf720bb71288c5e8b40733c7b9ea
Vicak66 <vicak614@gmail.com>
2024-10-07 14:09:21 +0630

    Algorithms

---
+++ fizz-buzz.js
@@ -0,0 +1,22 @@
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+
+
+
+var fizzBuzz = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var currentNumber = arr[i];
+
+        if (currentNumber % 15 === 0) {
+            console.log("Fizz Buzz");
+        } else if (currentNumber % 3 === 0) {
+            console.log("Fizz");
+        } else if (currentNumber % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(currentNumber);
+        }
+    }
+};
+
+fizzBuzz([6, 7, 8, 9, 10, 11, 12, 13, 14, 15,]);
